337bec64fd60964e16ea3cb26ab915687d211509
Mark Ryan <mark.ryan@getordermark.com>
2022-12-08 14:19:08 -0500

    Fix for pulling tracks with Various Artist as the album artist

--- app/app.py
+++ app/app.py
@@ -12,13 +12,16 @@ api_key = os.getenv("SCROBBLE_API_KEY")
 @app.route('/', methods=['POST'])
 def home():
     data = json.loads(request.form['payload'])
+    print(data)
+
     is_scrobble = data['event'] == 'media.scrobble'
     is_audio_track = data['Metadata']['type'] == 'track'
-
     if is_scrobble and is_audio_track:
         title = data['Metadata']['title']
         album = data['Metadata']['parentTitle']
         artist = data['Metadata']['grandparentTitle']
+        if artist == 'Various Artists':
+            artist = data['Metadata']['originalTitle']
         response = requests.post(scrobble_url, json={'artist': artist, 'album': album, 'title': title, 'key': api_key})
     return ''
 
